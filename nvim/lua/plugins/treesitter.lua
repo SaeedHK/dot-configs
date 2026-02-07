7d50a9b7ca5f26ccc579b4e8d9059ba635d604f2
Saeed Hadikhanloo <saeed.hadikhanloo@gmail.com>
2026-02-07 09:31:36 +0100

    feat (nvim): mac does not accept nvim treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
     config = function()
-      require("nvim-treesitter.configs").setup({
+      require("nvim-treesitter").setup({
         ensure_installed = {
           "lua",
           "vim",
@@ -26,21 +26,8 @@ return {
           "haskell",
         },
         auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<C-space>",
-            node_incremental = "<C-space>",
-            scope_incremental = false,
-            node_decremental = "<bs>",
-          },
-        },
       })
+      vim.api.nvim_set_hl(0, "@markup.raw.markdown_inline", {})
     end,
   },
 }
