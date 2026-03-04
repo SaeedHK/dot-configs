7e50b3e76627f16eba5fa921c99d683c5bffafec
Saeed Hadikhanloo <saeed.hadikhanloo@hrflow.ai>
2026-03-04 16:47:16 +0100

    feat(nvim): add move window keybinding in terminal mode

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -17,6 +17,10 @@ return {
 
       -- Esc enters normal mode (for navigation/selection), C-\ closes
       vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Enter normal mode in terminal" })
+
+      -- Window navigation from terminal mode
+      vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Move to lower window" })
+      vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Move to upper window" })
     end,
   },
 }
