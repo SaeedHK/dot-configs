2133bb54a67ecffff8ac831a1ed64af0d0c1fff5
Saeed Hadikhanloo <saeed.hadikhanloo@hrflow.ai>
2026-03-04 10:13:24 +0100

    feat(nvim): use standard telescope keybindings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -11,8 +11,8 @@ return {
     },
     cmd = "Telescope",
     keys = {
-      { "<leader>n", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-      { "<leader>f", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
       { "<leader>fo", "<cmd>Telescope oldfiles<cr>", desc = "Recent files" },
